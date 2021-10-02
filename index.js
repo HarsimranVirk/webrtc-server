2d4b0c3a374a39f2e28319adbae3c267049b315f
Harsimran Virk <harsimranvirk99@gmail.com>
2021-10-02 22:35:43 +0530

    PeerJS and Socketio

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*"
+  }
+});
+
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(httpServer, {
+  debug: true
+});
+
+const cors = require("cors");
+app.use(express.json());
+app.use(cors());
+
+let lives = [];
+
+app.use("/peer", peerServer);
+
+app.get("/live", (req, res) => {
+  res.json({ lives });
+});
+
+app.post("/live/:id", (req, res) => {
+  lives.push(req.params.id);
+  setTimeout(() =>
+    lives = lives.filter((live) => live !== req.params.id), 1000 * 60 * 30);
+  io.emit("livesUpdated", lives);
+  res.json({ lives });
+});
+
+app.delete("/live/:id", (req, res) => {
+  lives = lives.filter((live) => live !== req.params.id);
+  io.emit("livesUpdated", lives);
+  res.json({ lives });
+});
+
+httpServer.listen(80, () => {
+  console.log("Started listening at port 80...");
+});
